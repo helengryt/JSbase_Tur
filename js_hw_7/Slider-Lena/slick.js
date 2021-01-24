6eb6ae829094eb44af410d6c1c2d41196a432554
helengryt <73492138+helengryt@users.noreply.github.com>
2021-01-24 23:25:52 +0200

    HW-7 fully completed
    Slider added + modal-box put in folder

---
+++ js_hw_7/Slider-Lena/slick.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:true,
+		dots:true,
+		adaptiveHeight:true,
+		slidesToShow:1,
+		speed:1000,
+		easing:'linear',
+		autoplay:true,
+		autoplaySpeed:400,
+	});
+});
